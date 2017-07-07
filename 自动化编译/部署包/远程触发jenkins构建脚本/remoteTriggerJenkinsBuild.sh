4020f54b77a61c81bfff99b4aa242543d5466ec0
admin <admin>
2017-07-07 09:59:58 +0800

    ddd

--- 自动化编译/部署包/远程触发jenkins构建脚本/remoteTriggerJenkinsBuild.sh
+++ 自动化编译/部署包/远程触发jenkins构建脚本/remoteTriggerJenkinsBuild.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-svnPath=""
+
+#svn相关配置信息
+#如果有多个svn路径需要关注时，分别写在下面的数组项中
+svnPath[0]=""
+svnPath[1]=""
+svnPath[2]=""
+svnPath[3]=""
+svnPath[4]=""
+
 svnUserName=""
 svnUserPasswd=""
 
+#jenkins相关配置信息
 jenkinsPath=""
 jenkinsJobName=""
 jenkinsJobToken=""
 jenkinsUserName=""
 jenkinsUserToken=""
 
-scriptHome="/home/test"
+#脚本所在路径
+scriptHome=""
 
 function main()
 {
-newestRev=`svn info $svnPath --username=$svnUserName --password=$svnUserPasswd | grep "Last Changed Rev" | awk -F ": " '{print $2}'`
+newestRev=0
+for i in ${svnPath[@]}
+do
+	tmpRev=`svn info $i --username=$svnUserName --password=$svnUserPasswd | grep "Last Changed Rev" | awk -F ": " '{print $2}'`
+	((newestRev+=$tmpRev))
+done
+
 if [ -f $scriptHome/rev ]
 then
 	rev=`cat $scriptHome/rev`	
